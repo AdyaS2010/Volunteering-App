a6ec096a92b1773ce339017ec08c616baf43335e
AdyaS2010 <adya.sastry@gmail.com>
2024-09-02 17:24:21 -0500

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to VolunteerHub!</h1>
+      <p>Find and participate in meaningful volunteer work!</p>
+    </div>
+  );
+}
+
+export default Home;
