9566e5dd2dd69b449a0873324719c23b15789795
AdyaS2010 <adya.sastry@gmail.com>
2024-09-02 19:30:44 -0500

    Create Progress.js

---
+++ Progress.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Progress() {
+  return (
+    <div>
+      <h1>Your Progress</h1>
+      <p>Track your volunteering progress here.</p>
+    </div>
+  );
+}
+
+export default Progress;
